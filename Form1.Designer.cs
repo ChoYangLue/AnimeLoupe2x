7038f1dbc5340dc3bc8a0482c7cbb4a5b97ca124
Cheetack <ken.tsuchiya.7q@stu.hosei.ac.jp>
2020-05-12 20:43:16 +0900

    this is first commit save button fixme.

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace AnimeLoupe2x
+{
+    partial class AnimeLoupe2x
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogText = new System.Windows.Forms.TextBox();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.OutputGroup = new System.Windows.Forms.GroupBox();
+            this.outputPath = new System.Windows.Forms.TextBox();
+            this.InputGroup = new System.Windows.Forms.GroupBox();
+            this.inputPath = new System.Windows.Forms.TextBox();
+            this.AddAudioCheckBox = new System.Windows.Forms.CheckBox();
+            this.I2VCheckBox = new System.Windows.Forms.CheckBox();
+            this.Waifu2xCheckBox = new System.Windows.Forms.CheckBox();
+            this.V2ACheckBox = new System.Windows.Forms.CheckBox();
+            this.V2ICheckBox = new System.Windows.Forms.CheckBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.InputPathButton = new System.Windows.Forms.Button();
+            this.CommandGroup = new System.Windows.Forms.GroupBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.OutputGroup.SuspendLayout();
+            this.InputGroup.SuspendLayout();
+            this.CommandGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LogText
+            // 
+            this.LogText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogText.Location = new System.Drawing.Point(0, 320);
+            this.LogText.Multiline = true;
+            this.LogText.Name = "LogText";
+            this.LogText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LogText.Size = new System.Drawing.Size(1163, 398);
+            this.LogText.TabIndex = 0;
+            // 
+            // RunButton
+            // 
+            this.RunButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RunButton.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.RunButton.Location = new System.Drawing.Point(1078, 234);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(73, 80);
+            this.RunButton.TabIndex = 1;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1072, 312);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.tabPage1.Controls.Add(this.CommandGroup);
+            this.tabPage1.Controls.Add(this.OutputGroup);
+            this.tabPage1.Controls.Add(this.InputGroup);
+            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1064, 280);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "command setting";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // OutputGroup
+            // 
+            this.OutputGroup.Controls.Add(this.SaveButton);
+            this.OutputGroup.Controls.Add(this.outputPath);
+            this.OutputGroup.Location = new System.Drawing.Point(242, 128);
+            this.OutputGroup.Name = "OutputGroup";
+            this.OutputGroup.Size = new System.Drawing.Size(635, 72);
+            this.OutputGroup.TabIndex = 10;
+            this.OutputGroup.TabStop = false;
+            this.OutputGroup.Text = "File Output";
+            // 
+            // outputPath
+            // 
+            this.outputPath.Location = new System.Drawing.Point(6, 36);
+            this.outputPath.Name = "outputPath";
+            this.outputPath.Size = new System.Drawing.Size(529, 25);
+            this.outputPath.TabIndex = 6;
+            // 
+            // InputGroup
+            // 
+            this.InputGroup.Controls.Add(this.InputPathButton);
+            this.InputGroup.Controls.Add(this.inputPath);
+            this.InputGroup.Location = new System.Drawing.Point(242, 26);
+            this.InputGroup.Name = "InputGroup";
+            this.InputGroup.Size = new System.Drawing.Size(635, 77);
+            this.InputGroup.TabIndex = 9;
+            this.InputGroup.TabStop = false;
+            this.InputGroup.Text = "File Input";
+            // 
+            // inputPath
+            // 
+            this.inputPath.Location = new System.Drawing.Point(6, 36);
+            this.inputPath.Name = "inputPath";
+            this.inputPath.Size = new System.Drawing.Size(529, 25);
+            this.inputPath.TabIndex = 2;
+            // 
+            // AddAudioCheckBox
+            // 
+            this.AddAudioCheckBox.AutoSize = true;
+            this.AddAudioCheckBox.Location = new System.Drawing.Point(6, 139);
+            this.AddAudioCheckBox.Name = "AddAudioCheckBox";
+            this.AddAudioCheckBox.Size = new System.Drawing.Size(105, 22);
+            this.AddAudioCheckBox.TabIndex = 8;
+            this.AddAudioCheckBox.Text = "AddAudio";
+            this.AddAudioCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // I2VCheckBox
+            // 
+            this.I2VCheckBox.AutoSize = true;
+            this.I2VCheckBox.Location = new System.Drawing.Point(6, 110);
+            this.I2VCheckBox.Name = "I2VCheckBox";
+            this.I2VCheckBox.Size = new System.Drawing.Size(128, 22);
+            this.I2VCheckBox.TabIndex = 7;
+            this.I2VCheckBox.Text = "Image2Video";
+            this.I2VCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Waifu2xCheckBox
+            // 
+            this.Waifu2xCheckBox.AutoSize = true;
+            this.Waifu2xCheckBox.Location = new System.Drawing.Point(6, 81);
+            this.Waifu2xCheckBox.Name = "Waifu2xCheckBox";
+            this.Waifu2xCheckBox.Size = new System.Drawing.Size(91, 22);
+            this.Waifu2xCheckBox.TabIndex = 5;
+            this.Waifu2xCheckBox.Text = "Waifu2x";
+            this.Waifu2xCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // V2ACheckBox
+            // 
+            this.V2ACheckBox.AutoSize = true;
+            this.V2ACheckBox.Location = new System.Drawing.Point(6, 52);
+            this.V2ACheckBox.Name = "V2ACheckBox";
+            this.V2ACheckBox.Size = new System.Drawing.Size(127, 22);
+            this.V2ACheckBox.TabIndex = 4;
+            this.V2ACheckBox.Text = "Video2Audio";
+            this.V2ACheckBox.UseVisualStyleBackColor = true;
+            // 
+            // V2ICheckBox
+            // 
+            this.V2ICheckBox.AutoSize = true;
+            this.V2ICheckBox.Location = new System.Drawing.Point(6, 24);
+            this.V2ICheckBox.Name = "V2ICheckBox";
+            this.V2ICheckBox.Size = new System.Drawing.Size(128, 22);
+            this.V2ICheckBox.TabIndex = 3;
+            this.V2ICheckBox.Text = "Video2Image";
+            this.V2ICheckBox.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 28);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1064, 280);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "waifu2x setting";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // InputPathButton
+            // 
+            this.InputPathButton.Location = new System.Drawing.Point(541, 32);
+            this.InputPathButton.Name = "InputPathButton";
+            this.InputPathButton.Size = new System.Drawing.Size(75, 32);
+            this.InputPathButton.TabIndex = 3;
+            this.InputPathButton.Text = "Open";
+            this.InputPathButton.UseVisualStyleBackColor = true;
+            this.InputPathButton.Click += new System.EventHandler(this.InputPathButton_Click);
+            // 
+            // CommandGroup
+            // 
+            this.CommandGroup.Controls.Add(this.V2ICheckBox);
+            this.CommandGroup.Controls.Add(this.V2ACheckBox);
+            this.CommandGroup.Controls.Add(this.Waifu2xCheckBox);
+            this.CommandGroup.Controls.Add(this.AddAudioCheckBox);
+            this.CommandGroup.Controls.Add(this.I2VCheckBox);
+            this.CommandGroup.Location = new System.Drawing.Point(30, 26);
+            this.CommandGroup.Name = "CommandGroup";
+            this.CommandGroup.Size = new System.Drawing.Size(187, 174);
+            this.CommandGroup.TabIndex = 11;
+            this.CommandGroup.TabStop = false;
+            this.CommandGroup.Text = "Command";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(541, 33);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 28);
+            this.SaveButton.TabIndex = 7;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // AnimeLoupe2x
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1163, 719);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.RunButton);
+            this.Controls.Add(this.LogText);
+            this.Name = "AnimeLoupe2x";
+            this.Text = "AnimeLoupe2x";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AnimeLoupe2x_FormClosing);
+            this.Load += new System.EventHandler(this.AnimeLoupe2x_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.OutputGroup.ResumeLayout(false);
+            this.OutputGroup.PerformLayout();
+            this.InputGroup.ResumeLayout(false);
+            this.InputGroup.PerformLayout();
+            this.CommandGroup.ResumeLayout(false);
+            this.CommandGroup.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LogText;
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox inputPath;
+        private System.Windows.Forms.CheckBox V2ICheckBox;
+        private System.Windows.Forms.CheckBox V2ACheckBox;
+        private System.Windows.Forms.CheckBox Waifu2xCheckBox;
+        private System.Windows.Forms.TextBox outputPath;
+        private System.Windows.Forms.CheckBox I2VCheckBox;
+        private System.Windows.Forms.CheckBox AddAudioCheckBox;
+        private System.Windows.Forms.GroupBox OutputGroup;
+        private System.Windows.Forms.GroupBox InputGroup;
+        private System.Windows.Forms.Button InputPathButton;
+        private System.Windows.Forms.GroupBox CommandGroup;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace AnimeLoupe2x
+{
+    public partial class AnimeLoupe2x : Form
+    {
+        public AnimeLoupe2x()
+        {
+            InitializeComponent();
+        }
+
+
+		//ラベル操作のデリゲート
+		public delegate void MyEventHandler(string data);
+		public event MyEventHandler myEvent = null;
+		Process process = null;
+		bool exec_com_flag = true;
+
+        public struct Command
+        {
+            public string command;
+            public string option;
+            public void log()
+            {
+                Console.WriteLine("command: " + command);
+                Console.WriteLine("option: " + option);
+            }
+        }
+        string FFmpegPath = Directory.GetCurrentDirectory()+@"\Lib\ffmpeg\";
+        string Waifu2xPath = Directory.GetCurrentDirectory() + @"\Lib\waifu2x-caffe\";
+
+        public struct VideoInfo
+        {
+            public string fps;
+            public string bitrate;
+
+            public void log()
+            {
+                Console.WriteLine("video info");
+                Console.WriteLine("fps: "+fps);
+                Console.WriteLine("bitrate: "+bitrate);
+            }
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+			myEvent = new MyEventHandler(event_DataReceived);
+
+			//System.Environment.CurrentDirectory = @"C:\Users\";
+			Console.WriteLine(System.Environment.CurrentDirectory);
+
+			Task task = Task.Run(() => {
+				CommandThreadFunc();
+			});
+
+		}
+
+		private void myProcess_Exited(object sender, System.EventArgs e)
+		{
+			this.Invoke(myEvent, "end");
+			exec_com_flag = true;
+		}
+
+		void event_DataReceived(string data)
+		{
+			LogText.AppendText(data + "\r\n");
+			Console.WriteLine(data);// ［出力］ウィンドウに出力
+		}
+
+		void p_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+		{
+			this.Invoke(myEvent, e.Data);
+		}
+
+		void CommandThreadFunc()
+		{
+			// 現在のマネージドスレッドの識別子を表示
+			Console.WriteLine("スレッドID] => {0}", Thread.CurrentThread.ManagedThreadId);
+
+			if (inputPath == null)
+			{
+				Console.WriteLine("file input is none.");
+				return;
+			}
+			if (outputPath == null)
+			{
+				Console.WriteLine("file output is none.");
+				return;
+			}
+
+            string dirpath = Directory.GetCurrentDirectory();
+            Console.WriteLine(dirpath);
+
+            string inputFilePath = inputPath.Text;
+			string outputFilePath = outputPath.Text;
+
+            string baseTempPath = Directory.GetCurrentDirectory() + @"\temp\";
+            string imageTempPath = baseTempPath+@"image\";
+            string audioTempPath = baseTempPath+@"audio\";
+            string videoTempPath = baseTempPath+@"video\";
+
+            if (Directory.Exists(baseTempPath) == false)
+            {
+                Directory.CreateDirectory(baseTempPath);
+                Directory.CreateDirectory(imageTempPath);
+                Directory.CreateDirectory(audioTempPath);
+                Directory.CreateDirectory(videoTempPath);
+            }
+
+
+            Command meirei;
+
+            VideoInfo video_info = new VideoInfo();
+            GetVideoInfo(inputFilePath, ref video_info);
+            video_info.log();
+
+            if (V2ICheckBox.Checked)
+			{
+				meirei = MakeVideo2ImageString(inputFilePath, imageTempPath+ "image_%08d.png");
+				ExecFFmpegCommand(meirei);
+			}
+			if (V2ACheckBox.Checked)
+			{
+				meirei = MakeSepAudioString(inputFilePath, audioTempPath + "audio1.aac");
+				ExecFFmpegCommand(meirei);
+			}
+			if (Waifu2xCheckBox.Checked)
+			{
+				meirei = MakeWaifu2xString();
+			}
+			if (I2VCheckBox.Checked)
+			{
+				meirei = MakeImage2VideoString(imageTempPath+ "image_%08d.png", videoTempPath+ "video_output.avi", video_info);
+				ExecFFmpegCommand(meirei);
+			}
+			if (AddAudioCheckBox.Checked)
+			{
+				meirei = MakeComAudioString(videoTempPath + "video_output.avi", audioTempPath + "audio1.aac", outputFilePath);
+				ExecFFmpegCommand(meirei);
+			}
+
+
+		}
+
+
+        Command MakeSepAudioString(string videoPath, string audioPath)
+		{
+			Command ret_val = new Command();
+			ret_val.command = FFmpegPath + "ffmpeg.exe";
+			ret_val.option = "-i "+ videoPath + " -vn -acodec copy "+ audioPath;
+			return ret_val;
+		}
+
+		Command MakeComAudioString(string baseVideoPath, string audioPath, string outVideoPath)
+		{
+			Command ret_val = new Command();
+			ret_val.command = FFmpegPath + "ffmpeg.exe";
+			//ret_val.option = @"-i F:\Program\C#\AnimeLoupe2x\temp\video_output.avi -i F:\Program\C#\AnimeLoupe2x\temp\audio.aac -c:v copy F:\Program\C#\AnimeLoupe2x\temp\output1.mp4";
+			ret_val.option = @"-i "+ baseVideoPath + " -i "+audioPath+" -c copy -map 0:v:0 -map 1:a:0 "+outVideoPath;
+			return ret_val;
+		}
+
+		Command MakeVideo2ImageString(string videoPath, string tempPath)
+		{
+			Command ret_val = new Command();
+			ret_val.command = FFmpegPath + "ffmpeg.exe";
+			ret_val.option = @"-i "+ videoPath + " -vcodec png "+tempPath;
+			//ret_val.option = @"-i F:\Program\C#\AnimeLoupe2x\input.mp4";
+			return ret_val;
+		}
+
+		Command MakeImage2VideoString(string imagePath, string videoPath, VideoInfo vi)
+		{
+			Command ret_val = new Command();
+			ret_val.command = FFmpegPath + "ffmpeg.exe";
+			ret_val.option = @"-framerate " + vi.fps + @" -i "+ imagePath + " -vcodec libx264 -pix_fmt yuv420p "+"-b "+ vi.bitrate + " -r "+vi.fps+" "+ videoPath;
+			return ret_val;
+		}
+
+		Command MakeWaifu2xString()
+		{
+			Command ret_val = new Command();
+			ret_val.command = Waifu2xPath + "waifu2x-caffe-cui.exe";
+			ret_val.option = @"-i F:\Program\C#\AnimeLoupe2x\temp\image_%08d.png -vcodec mjpeg -q 0 F:\Program\C#\AnimeLoupe2x\temp\video_output.avi";
+			return ret_val;
+		}
+
+
+		int ExecFFmpegCommand(Command meirei)
+		{
+            meirei.log();
+			Process proc = new Process();
+			proc.StartInfo.FileName = meirei.command;
+			proc.StartInfo.Arguments = meirei.option;
+
+			proc.StartInfo.RedirectStandardError = true; // 標準出力をリダイレクト
+			proc.StartInfo.UseShellExecute = false; // シェル機能オフ
+			proc.StartInfo.CreateNoWindow = true; // コマンドプロンプトを非表示
+			if (!proc.Start())
+			{
+				Console.WriteLine("Error starting");
+				return -1;
+			}
+			StreamReader reader = proc.StandardError;
+			string line;
+			while ((line = reader.ReadLine()) != null)
+			{
+				this.Invoke(myEvent, line);
+
+			}
+			proc.Close();
+			this.Invoke(myEvent, "END command");
+			return 0;
+		}
+
+        int GetVideoInfo(string videoPath, ref VideoInfo vi)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = FFmpegPath + "ffmpeg.exe";
+            proc.StartInfo.Arguments = "-i " + videoPath;
+
+            proc.StartInfo.RedirectStandardError = true; // 標準出力をリダイレクト
+            proc.StartInfo.UseShellExecute = false; // シェル機能オフ
+            proc.StartInfo.CreateNoWindow = true; // コマンドプロンプトを非表示
+            if (!proc.Start())
+            {
+                Console.WriteLine("Error starting");
+                return -1;
+            }
+            StreamReader reader = proc.StandardError;
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                this.Invoke(myEvent, line);
+                if (line.IndexOf("fps") > 0)
+                {
+                    vi.fps = line.Substring(line.IndexOf("fps") - 6, 5);
+                }
+                if (line.IndexOf("bitrate:") > 0)
+                {
+                    string bitrate_tmp = line.Substring(line.IndexOf("bitrate:") + 9, 6);
+                    vi.bitrate = bitrate_tmp.Replace(" ", "");
+                } 
+
+            }
+            proc.Close();
+
+            return 0;
+        }
+
+
+		int ExecWaifu2xCommand()
+		{
+			
+// List<string> str
+for (int i = 0; i < 1; i++)
+{
+	while (exec_com_flag == false)
+	{
+		Console.WriteLine("waiting");
+		Thread.Sleep(1000);
+	}
+
+	process = new Process();
+
+	process.StartInfo.FileName = @"F:\Program\C#\AnimeLoupe2x\ffmpeg.exe";
+	process.StartInfo.Arguments = @"-i F:\Program\C#\AnimeLoupe2x\input.mp4 -vcodec png F:\Program\C#\AnimeLoupe2x\temp\image_%05d.png";
+
+	process.StartInfo.UseShellExecute = false; // シェル機能オフ
+	process.StartInfo.CreateNoWindow = true; // コマンドプロンプトを非表示
+	process.StartInfo.RedirectStandardOutput = true; // 標準出力をリダイレクト
+	process.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+	process.StartInfo.Arguments = @"-i F:\Program\C#\AnimeLoupe2x\input.mp4 -vcodec png F:\Program\C#\AnimeLoupe2x\temp\image_%05d.png";
+	process.EnableRaisingEvents = true;
+	process.Exited += new EventHandler(myProcess_Exited);
+
+	process.Start();
+	process.BeginOutputReadLine();
+	exec_com_flag = false;
+}
+			return 0;
+		}
+
+		private void AnimeLoupe2x_Load(object sender, EventArgs e)
+		{
+			inputPath.Text = Properties.Settings.Default.input_path;
+			outputPath.Text = Properties.Settings.Default.output_path;
+
+			V2ICheckBox.Checked = Properties.Settings.Default.video2image_flag;
+			V2ACheckBox.Checked = Properties.Settings.Default.video2audio_flag;
+			Waifu2xCheckBox.Checked = Properties.Settings.Default.waifu2x_flag;
+			I2VCheckBox.Checked = Properties.Settings.Default.image2video_flag;
+			AddAudioCheckBox.Checked = Properties.Settings.Default.add_audio_flag;
+        }
+
+		private void AnimeLoupe2x_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			Properties.Settings.Default.input_path = inputPath.Text;
+			Properties.Settings.Default.output_path = outputPath.Text;
+
+			Properties.Settings.Default.video2image_flag = V2ICheckBox.Checked;
+			Properties.Settings.Default.video2audio_flag = V2ACheckBox.Checked;
+			Properties.Settings.Default.waifu2x_flag = Waifu2xCheckBox.Checked;
+			Properties.Settings.Default.image2video_flag = I2VCheckBox.Checked;
+			Properties.Settings.Default.add_audio_flag = AddAudioCheckBox.Checked;
+
+            // ファイルに保存
+            Properties.Settings.Default.Save();
+        }
+
+        private void InputPathButton_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定する
+            //はじめに「ファイル名」で表示される文字列を指定する
+            ofd.FileName = System.IO.Path.GetFileName(inputPath.Text);
+            //はじめに表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            ofd.InitialDirectory = System.IO.Path.GetDirectoryName(inputPath.Text);
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter = "mp4ファイル(*.mp4)|*.mp4|すべてのファイル(*.*)|*.*";
+            //[ファイルの種類]ではじめに選択されるものを指定する
+            //2番目の「すべてのファイル」が選択されているようにする
+            ofd.FilterIndex = 2;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                Console.WriteLine(ofd.FileName);
+                inputPath.Text = ofd.FileName;
+            }
+        }
+    }
+}
